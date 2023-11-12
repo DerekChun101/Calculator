63b8dfe30b690b71d193484628176a14459055b8
Derek Chun <derekchun2013@gmail.com>
2023-11-12 13:10:02 -0500

    Created operate function operate function takes in num1 , num2 and a operator. It will then call a function using said arguments

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 function add (x ,y) {
+    console.log(x + y);
     return x + y;
+    
 }
 
 function subtract(x, y) {
+    console.log(x - y);
     return x - y;
 }
 
 function multiply(x ,y) {
+    console.log(x * y);
     return x * y;
 }
 
 function divide(x, y) {
+    console.log(x / y);
     return x/y;
 }
 
-const num1 = '';
-const num2 = '';
-const operator = '';
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case 'add':
+            add(num1, num2);
+            break;
+        case 'subtract':
+            subtract(num1, num2);
+            break;
+        case 'multiply':
+            multiply(num1, num2);
+            break;
+        case 'divide':
+            divide(num1, num2);
+    }
+}
+
+const num1 = 1;
+const num2 = 2;
+const operator = 'add';
