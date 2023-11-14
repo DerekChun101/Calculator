6caebeb0d26be93f3a98660d6413432f94eea184
Derek Chun <derekchun2013@gmail.com>
2023-11-13 19:00:57 -0500

    Switches to second input after operator button

--- script.js
+++ script.js
@@ -36,17 +36,44 @@ function operate (num1, num2, operator) {
     }
 }
 
+function nextNum () {
+    digits.forEach(button => {
+        button.onclick = () => {
+            num2 += button.textContent;
+            display.textContent = num2;
+            console.log('num1 :' + num1);
+            console.log('num2 :' + num2);
+        }
+    });                                                                            
+    
+}
+// function updateDisplay(text) {
+//     num1 += this.textContent;
+//     console.log(num1);
+//     display.textContent = num1;
+//     isFirstNum = false;
+// }
+
+
 let num1 = '';
 let num2 = '';
 const operator = 'add';
-
+let isFirstNum = 'true';
 const digits = document.querySelectorAll('.digitsButton');
 const display = document.querySelector('.display');
 digits.forEach(button => {
-    button.addEventListener('click', () => {
+    button.onclick = () => {
         num1 += button.textContent;
         console.log(num1);
         display.textContent = num1;
+        isFirstNum = false;
+    }
+});
+const operators = document.querySelectorAll('.operatorsButtons'); 
+operators.forEach(button => {
+    button.addEventListener('click', () => {
+        if(isFirstNum === false) {
+            nextNum();
+        }
     })
 });
-console.log(digits); 
