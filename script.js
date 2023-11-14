5bb0a6c354cfe2c37d61c28fffd6a0ebb37e16c3
Derek Chun <derekchun2013@gmail.com>
2023-11-13 23:54:10 -0500

    small change

--- script.js
+++ script.js
@@ -126,7 +126,7 @@ equals.addEventListener('click', () => {
     }
 });
 const clear = document.querySelector('.clear');
-clear.addEventListener('click', () => {
+clear.addEventListener('click', () => {g
     display.textContent = '';
     resetNums();
     backToOne();
